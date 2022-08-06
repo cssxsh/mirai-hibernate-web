526edfc4965e304a8f1c6055bcd5c404dfd27782
dousha <lijiahao34@live.com>
2022-08-07 01:55:15 +0800

    refactoring routes

--- src/logic/Routes.ts
+++ src/logic/Routes.ts
@@ -1,4 +1,4 @@
-import { MessageRecord } from "../types";
+import { isKindFilterOptions, KindFilterOptions, MessageFilterOptions, MessageRecord } from '../types';
 
 export const bot = async function (bot: number, start: number, end: number) {
 	const response = await fetch(`/message/bot?bot=${bot}&start=${start}&end=${end}`);
@@ -28,9 +28,18 @@ export const member = async function (bot: number, group: number, member: number
 	return data as MessageRecord[]
 }
 
-export const kind = async function (kind: string, start: number, end: number) {
-	const response = await fetch(`/message/kind?kind=${kind}&start=${start}&end=${end}`);
+async function fetchMessagesByKind(filter: KindFilterOptions): Promise<Array<MessageRecord>> {
+	const response = await fetch(`/message/kind?kind=${filter.kind}&start=${filter.from}&end=${filter.to}`);
 	const data = await response.json()
 
 	return data as MessageRecord[]
+}
+
+export async function fetchMessages(filter: MessageFilterOptions): Promise<Array<MessageRecord>> {
+	// TODO
+	if (isKindFilterOptions(filter)) {
+		return fetchMessagesByKind(filter);
+	}
+
+	return [];
 }
--- src/types.ts
+++ src/types.ts
@@ -1,17 +1,85 @@
-import { kind, bot, group, friend, member } from './logic/Routes';
+export enum FilterType {
+	ByKind = 'KIND',
+	ByBot = 'BOT',
+	ByGroup = 'GROUP',
+	ByFriend = 'FRIEND',
+	ByMember = 'MEMBER',
+}
+
+export function toFilterType(s: string): FilterType {
+	const coercedFilterType = FilterType as any;
+	for (let k in coercedFilterType) {
+		if (coercedFilterType[k] == s) {
+			return coercedFilterType[k] as FilterType;
+		}
+	}
+	return FilterType.ByKind;
+}
 
 export type SearchOption = {
+	value: FilterType;
 	label: string;
-	load: (...xs: any[]) => Promise<MessageRecord[]>;
 }
 
 export const SearchOptions: SearchOption[] = [
-	{label: "KIND", load: kind},
-	{label: "BOT", load: bot},
-	{label: "GROUP", load: group},
-	{label: "FRIEND", load: friend},
-	{label: "MEMBER", load: member},
+	{value: FilterType.ByKind, label: "按消息类型"},
+	{value: FilterType.ByBot, label: "按机器人 ID"},
+	{value: FilterType.ByGroup, label: "按群号"},
+	{value: FilterType.ByFriend, label: "按 QQ 号"},
+	{value: FilterType.ByMember, label: "按群成员"},
 ];
+
+export enum KindFilterType {
+	GroupMessages = 'GROUP',
+	FriendMessages = 'FRIEND',
+	GroupDirectMessage = 'TEMP',
+	StrangerDirectMessage = 'STRANGER',
+}
+
+interface TimeBoundOptions {
+	from: Timestamp;
+	to: Timestamp;
+}
+
+interface BotBoundOptions {
+	bot: BotId;
+}
+
+export interface KindFilterOptions extends TimeBoundOptions {
+	kind: KindFilterType;
+}
+
+export function isKindFilterOptions(o: MessageFilterOptions): o is KindFilterOptions {
+	return 'kind' in o && 'from' in o && 'to' in o && isFinite(o.from) && isFinite(o.to);
+}
+
+export interface BotFilterOptions extends TimeBoundOptions, BotBoundOptions {
+	// no member
+}
+
+export interface GroupMessageFilterOptions extends TimeBoundOptions, BotBoundOptions {
+	group: GroupId;
+}
+
+export interface DirectMessageFilterOptions extends TimeBoundOptions, BotBoundOptions {
+	account: AccountId;
+}
+
+export type Timestamp = number;
+
+export type BotId = number;
+
+export type GroupId = number;
+
+export type AccountId = number;
+
+export type MessageFilterOptions
+	= KindFilterOptions
+	| BotFilterOptions
+	| GroupMessageFilterOptions
+	| DirectMessageFilterOptions;
+
+export type PartialMessageFilterOptions = Omit<MessageFilterOptions, keyof TimeBoundOptions>
 
 export type MessageRecord = {
 	id: number
