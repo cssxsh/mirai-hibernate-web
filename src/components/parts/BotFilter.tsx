44622f7d56897e80da9b79db7f4b67b387952c15
dousha <lijiahao34@live.com>
2022-08-07 04:09:32 +0800

    add bot filter control

--- src/components/parts/BotFilter.tsx
+++ src/components/parts/BotFilter.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { PartialMessageFilterOptions } from '../../types';
 import { Grid, TextField } from '@mui/material';
 
@@ -7,9 +7,26 @@ export interface BotFilterProps {
 }
 
 export default function BotFilter(props: BotFilterProps) {
+	const [value, setValue] = useState('');
+
+	const updateValue = () => {
+		console.log(value);
+		if (value.length > 0) {
+			try {
+				const id = Number(value);
+				props.onValueChange({bot: id});
+			} catch (_) { }
+		}
+	};
+
+	useEffect(() => {
+		const t = setTimeout(updateValue, 350);
+		return () => clearTimeout(t);
+	}, [value]);
+
 	return (<>
 		<Grid item xs={3}>
-			<TextField id={'ipt-bot-id'} label={'机器人 QQ 号'} fullWidth />
+			<TextField id={'ipt-bot-id'} label={'机器人 QQ 号'} fullWidth inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} value={value} onChange={(e) => {setValue(e.target.value);}} />
 		</Grid>
 	</>);
 }
