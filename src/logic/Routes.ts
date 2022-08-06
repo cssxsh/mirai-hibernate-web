f85d2ac8f04fde9bd662ad2c11e89162597f94e4
dousha <lijiahao34@live.com>
2022-08-07 04:17:37 +0800

    duplication clean up

--- src/logic/Routes.ts
+++ src/logic/Routes.ts
@@ -9,39 +9,31 @@ import {
 	MessageRecord, TemporaryMessageFilterOptions,
 } from '../types';
 
-async function fetchMessagesByKind(filter: KindFilterOptions): Promise<Array<MessageRecord>> {
-	const response = await fetch(`/message/kind?kind=${filter.kind}&start=${filter.from}&end=${filter.to}`);
+async function fetchMessage(url: string): Promise<Array<MessageRecord>> {
+	const response = await fetch(url);
 	const data = await response.json();
 
 	return data as MessageRecord[];
+}
+
+async function fetchMessagesByKind(filter: KindFilterOptions): Promise<Array<MessageRecord>> {
+	return fetchMessage(`/message/kind?kind=${filter.kind}&start=${filter.from}&end=${filter.to}`);
 }
 
 async function fetchMessagesByBot(filter: BotFilterOptions): Promise<Array<MessageRecord>> {
-	const response = await fetch(`/message/bot?bot=${filter.bot}&start=${filter.from}&end=${filter.to}`);
-	const data = await response.json();
-
-	return data as MessageRecord[];
+	return fetchMessage(`/message/bot?bot=${filter.bot}&start=${filter.from}&end=${filter.to}`);
 }
 
 async function fetchMessagesByGroup(filter: GroupMessageFilterOptions): Promise<Array<MessageRecord>> {
-	const response = await fetch(`/message/group?bot=${filter.bot}&group=${filter.group}&start=${filter.from}&end=${filter.to}`);
-	const data = await response.json();
-
-	return data as MessageRecord[];
+	return fetchMessage(`/message/group?bot=${filter.bot}&group=${filter.group}&start=${filter.from}&end=${filter.to}`);
 }
 
 async function fetchMessagesByFriend(filter: DirectMessageFilterOptions): Promise<Array<MessageRecord>> {
-	const response = await fetch(`/message/group?bot=${filter.bot}&friend=${filter.account}&start=${filter.from}&end=${filter.to}`);
-	const data = await response.json();
-
-	return data as MessageRecord[];
+	return fetchMessage(`/message/group?bot=${filter.bot}&friend=${filter.account}&start=${filter.from}&end=${filter.to}`);
 }
 
 async function fetchMessagesByTemporarySession(filter: TemporaryMessageFilterOptions): Promise<Array<MessageRecord>> {
-	const response = await fetch(`/message/member?bot=${filter.bot}&group=${filter.group}&member=${filter.account}&start=${filter.from}&end=${filter.to}`);
-	const data = await response.json();
-
-	return data as MessageRecord[];
+	return fetchMessage(`/message/member?bot=${filter.bot}&group=${filter.group}&member=${filter.account}&start=${filter.from}&end=${filter.to}`);
 }
 
 export async function fetchMessages(filter: MessageFilterOptions): Promise<Array<MessageRecord>> {
